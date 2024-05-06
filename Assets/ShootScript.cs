63b20c90db0ff65878d69d83a63549ae0479b190
Liam Davis <lfd190000@utdallas.edu>
2024-05-05 22:42:33 -0500

    Added more advance Shoot Settings
    Developer in now able to change settings within Shoot Script, change Rate of Fire, Enable Limited Ammo, Change Max Ammo, Enable Reloading, Change Reload Speed.

--- Assets/ShootScript.cs
+++ Assets/ShootScript.cs
@@ -8,15 +8,77 @@ public class ShootScript : MonoBehaviour
     [SerializeField] bool _limitedAmmo = false;
     [SerializeField] int _maxAmmo = 10;
     [SerializeField] bool _canReload = false;
-    [SerializeField] float _reloadSpeed = 1f;
+    [SerializeField] float _reloadSpeed = 5f;
     [SerializeField] GameObject _projectile = null;
 
     bool canShoot = true;
+    float coolDown = 1;
+    int CurrentAmmo = 10;
+    float reloadTimer = 5f;
+    bool reloading = false;
+    private void Start()
+    {
+        coolDown = _rateOfFire;
+        CurrentAmmo = _maxAmmo;
+        reloadTimer = _reloadSpeed;
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) && canShoot == true)
         {
-            GameObject nprojectile = Instantiate(_projectile, transform.position, transform.rotation);
+            Shoot();
         }
+        if(_limitedAmmo == false)
+        {
+            if (canShoot == false)
+            {
+                coolDown -= Time.deltaTime;
+                if(coolDown <= 0)
+                {
+                    canShoot = true;
+                    coolDown = _rateOfFire;
+                }
+            }
+        }
+        else if (_limitedAmmo == true)
+        {
+                if(CurrentAmmo > 0)
+                {
+                    if (canShoot == false)
+                    {
+                        coolDown -= Time.deltaTime;
+                        if (coolDown <= 0)
+                        {
+                            canShoot = true;
+                            coolDown = _rateOfFire;
+                        }
+                    }
+                }
+        }
+        if (Input.GetKeyDown(KeyCode.R) && _canReload == true)
+        {
+            Debug.Log("Reload");
+            reloading = true;
+        }
+        if(reloading == true)
+        {
+            reloadTimer -= Time.deltaTime;
+            if (reloadTimer <= 0)
+            {
+                Debug.Log("Reloaded");
+                CurrentAmmo = _maxAmmo;
+                reloadTimer = _reloadSpeed;
+                reloading = false;
+            }
+        }
+ 
+    }
+
+    private void Shoot()
+    {
+        GameObject nprojectile = Instantiate(_projectile, transform.position, transform.rotation);
+        canShoot = false;
+        CurrentAmmo -= 1;
+        
     }
 }
